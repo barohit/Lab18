dcdd8810c3d28d617a8b34b3af8db97cfc6391b2
Rohit <rohit.r.baddam@gmail.com>
2019-05-16 15:55:07 -0400

    added more tests

--- src/PrimeNumberLocatorTest.java
+++ src/PrimeNumberLocatorTest.java
@@ -4,10 +4,46 @@ import org.junit.jupiter.api.Test;
 
 public class PrimeNumberLocatorTest {
 
+	
 	@Test
 	void numTest() {
-		// assertEquals(13, PrimeNumberLocator.findNthPrime(6));
-		assertEquals(401, PrimeNumberLocator.findNthPrime(79)); 
+		assertEquals(5, PrimeNumberLocator.findNthPrime(3)); 
+		
+		
 	}
+	
+	@Test
+	void numTest2() {
+		assertEquals(13, PrimeNumberLocator.findNthPrime(6));
+		
+		
+	}
+	
+	@Test
+	void numTest3() {
+		assertEquals(59, PrimeNumberLocator.findNthPrime(17)); 
+		
+		
+	}
+	
+	@Test
+	void numTest4() {
+		assertEquals(401, PrimeNumberLocator.findNthPrime(79)); 
+		
+		
+	}
+	
+	@Test
+	void numTest5() {
+		assertEquals(997, PrimeNumberLocator.findNthPrime(168)); 
+		
+		
+	}
+	
+	
+	
+	
+	
+	
 
 }
